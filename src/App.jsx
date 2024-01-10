031d2f119fbeee4a474d5f1982cf7d9e912524e4
Vaidas <vaidas.makstutis@gmail.com>
2024-01-10 20:37:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-center font-bold  text-green-700 pt-8">Welcome to Vite with TailwindCSS and React</h1>
+    </>
+  );
+};
+
+export default App;
